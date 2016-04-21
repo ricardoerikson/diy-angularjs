16fc6cd0dfb6b7825b8083008d20f361bae1a5f0
Ricardo Erikson <ricardoerikson@gmail.com>
2016-04-21 18:57:24 -0400

    Integrating code with the digest cycle

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 var Scope = require('./scope');
 
 var scope = new Scope();
-scope.number = 1;
+scope.counter = 0;
 
-scope.$eval(function(scope) {
-    console.log('Number during $eval: ', scope.number);
+scope.$watch(
+    function(scope) {
+        return scope.aValue;
+    },
+    function(newValue, oldValue, scope) {
+        scope.counter++;
+    }
+);
+
+scope.$apply(function(scope) {
+    scope.aValue = 'Hello from "outside"';
 });
+
+console.log(scope.counter === 1); // true
+console.log(scope.aValue); // Hello from "outside"
