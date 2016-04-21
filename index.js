48b21af9dcf630536fbe2e26493e5cc3f7ffbad1
Ricardo Erikson <ricardoerikson@gmail.com>
2016-04-21 18:10:15 -0400

    Evaluating code in the context of a scope

--- index.js
+++ index.js
@@ -1,21 +1,8 @@
 var Scope = require('./scope');
 
 var scope = new Scope();
-scope.number = 0;
-scope.counter = 0;
+scope.number = 1;
 
-scope.$watch(
-    function(scope){
-        return scope.number;
-    },
-    function(newValue, oldValue, scope){
-        scope.counter++;
-    }
-);
-
-scope.$digest();
-console.log(scope.counter === 1); // true
-
-scope.number = parseInt('wat', 10); // Becomes NaN
-scope.$digest();
-console.log(scope.counter === 2); // true
+scope.$eval(function(scope) {
+    console.log('Number during $eval: ', scope.number);
+});
